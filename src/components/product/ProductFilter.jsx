6c75835bc61ab1acf274ca2f9cad1aeab24218bd
HarikrushnR <harikrushnr.codetown@gmail.com>
2023-02-10 13:04:17 +0530

    changes

--- src/components/product/ProductFilter.jsx
+++ src/components/product/ProductFilter.jsx
@@ -16,6 +16,8 @@ const ProductPageFilter = ({ filters, swipeableIndex, selectedProduct, selectedI
     const [filterList, setFilterList] = useState([]);
     const [attributeList, setAttributeList] = useState([]);
     const [attributeData, setAttributeData] = useState({});
+    const [qty, setQty] = useState(1);
+    const [isCartAdd, setIsCartAdd] = useState(false);
     const [addToCart, { isLoading }] = useAddToCartMutation()
     const dispatch = useDispatch()
 
@@ -39,32 +41,55 @@ const ProductPageFilter = ({ filters, swipeableIndex, selectedProduct, selectedI
     const handleAdd = async () => {
         const selectedData = filterList?.find(list => list?._id == Object.values(attributeData)?.filter(list => list != 'defaultValue')?.slice(-1)?.[0]) ?? {}
         const cartData = JSON.parse(Storage.get("cartData")) ?? []
-        console.log("selectedData", selectedData)
         if (Storage.isUserAuthenticated()) {
             await addToCart({
                 cart: [{
                     sku: selectedData?._id,
                     product: selectedData?.product_id,
-                    qty: 1,
+                    qty,
                     amount: selectedProduct?.sale_price
                 }]
-            }).unwrap().then((data) => {
+            }).unwrap().then(() => {
+                setIsCartAdd(true)
+                setTimeout(() => {
+                    setIsCartAdd(false)
+                }, 1000)
             }).catch((error) => toast.error(error?.data?.message))
         } else {
-            const finalData = [...cartData, {
-                sku: {
-                    _id: selectedData?._id,
-                    product_name: selectedData?.product_name,
-                    sku: selectedData?.sku,
-                    varients: selectedData?.varients,
-                },
-                image: selectedImage ?? '',
-                product: selectedData?.product_id,
-                qty: 1,
-                amount: selectedProduct?.sale_price
-            }]
+            let finalData = [...cartData]
+            if (cartData?.some(x => x?.sku?._id == selectedData?._id)) {
+                let indexData = cartData?.findIndex(x => x?.sku?._id == selectedData?._id)
+                finalData[indexData] = { ...finalData[indexData], qty: finalData?.[indexData]?.qty + 1 }
+            } else {
+                finalData = [...finalData, {
+                    sku: {
+                        _id: selectedData?._id,
+                        product_name: selectedData?.product_name,
+                        sku: selectedData?.sku,
+                        varients: selectedData?.varients,
+                    },
+                    image: selectedImage ?? '',
+                    product: selectedData?.product_id,
+                    qty,
+                    amount: selectedProduct?.sale_price
+                }]
+            }
             Storage.set("cartData", JSON.stringify(finalData))
             dispatch(setCartCount(finalData?.length))
+            setIsCartAdd(true)
+            setTimeout(() => {
+                setIsCartAdd(false)
+            }, 1000)
+        }
+    }
+
+    const handleIncrement = async () => {
+        setQty(qty + 1)
+    }
+
+    const handleDecrement = async () => {
+        if (qty > 1) {
+            setQty(qty - 1)
         }
     }
 
@@ -74,6 +99,7 @@ const ProductPageFilter = ({ filters, swipeableIndex, selectedProduct, selectedI
                 <div className='product_filters_wrap'>
                     <div className="product_filters_wrap product_page_filtter">
                         {Object.keys(attributeList)?.map((item, index) => {
+                            console.log(attributeList, "attributeList", attributeData)
                             return (
                                 <>
                                     <div
@@ -104,7 +130,7 @@ const ProductPageFilter = ({ filters, swipeableIndex, selectedProduct, selectedI
                                                     </div>
                                                 </MenuItem>
                                                 {index == 0 ?
-                                                    attributeList?.[item]?.map((list) => {
+                                                    _.uniqBy(attributeList?.[item], x => x?.label)?.map((list) => {
                                                         return (
                                                             <MenuItem value={list?.value} className="common_option_wrap">
                                                                 <div className="common_option">
@@ -120,7 +146,7 @@ const ProductPageFilter = ({ filters, swipeableIndex, selectedProduct, selectedI
                                                         )
                                                     })
                                                     :
-                                                    attributeList?.[item]?.filter(list => list?.value == attributeData?.[Object.keys(attributeList)?.[index - 1]])?.map((list) => {
+                                                    attributeList?.[item]?.filter(list => Object.values(attributeList)?.[index - 1]?.filter(x => x?.label == Object.values(attributeList)?.[index - 1]?.find(list => list?.value == attributeData[Object.keys(attributeList)?.[index - 1]])?.label)?.map(x => x?.value)?.includes(list?.value))?.map((list) => {
                                                         return (
                                                             <MenuItem value={list?.value} className="common_option_wrap">
                                                                 <div className="common_option">
@@ -142,40 +168,69 @@ const ProductPageFilter = ({ filters, swipeableIndex, selectedProduct, selectedI
                                 </>
                             )
                         })}
-                        <div className='display_none_in_mobile' style={{
+                        <div className='quantiy_wrapper' style={{
                             margin: '0 10px 0 10px'
                         }}>
-                            <input type="text" placeholder='Qty' defaultValue="1" style={{
-                                maxWidth: '100px'
-                            }} />
+                            {/* <p>Qty</p> */}
+                            <div className='quantiy_inner'>
+                                <button type='button' className='common_btn' onClick={handleDecrement} >
+                                    <svg width="9" height="10" viewBox="0 0 9 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g clipPath="url(#clip0_160_1440)">
+                                            <path d="M8.14 5H0" stroke="black" strokeWidth="1.7" strokeMiterlimit="10" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_160_1440">
+                                                <rect width="8.14" height="8.14" fill="#2A3592" transform="translate(0 0.929932)" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </button>
+                                <input type="text" value={qty} />
+                                <button type='button' className='common_btn' onClick={handleIncrement} >
+                                    <svg width="9" height="10" viewBox="0 0 9 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g clipPath="url(#clip0_160_1444)">
+                                            <path d="M4.20966 0.929932V9.06993" stroke="black" strokeWidth="1.7" strokeMiterlimit="10" />
+                                            <path d="M8.27965 5H0.139648" stroke="black" strokeWidth="1.7" strokeMiterlimit="10" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_160_1444">
+                                                <rect width="8.14" height="8.14" fill="#2A3592" transform="translate(0.139648 0.929932)" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
                         <div className="add_btn_wrap">
-                            {/* <button className="clear_btn add_btn">
-                                <span>Added to Cart</span>
-                                <span>
-                                    <svg
-                                        width="10"
-                                        height="8"
-                                        viewBox="0 0 10 8"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            d="M9.40005 0.960205L3.92005 6.44021L1.30005 3.82021"
-                                            stroke="white"
-                                            strokeWidth="1.7"
-                                            strokeMiterlimit="10"
-                                        />
-                                    </svg>
-                                </span>
-                            </button> */}
-                            <button className={`clear_btn add_btn ${Object.values(attributeData)?.some(list => list == 'defaultValue') ? 'disabled-btn' : ''}`} disabled={Object.values(attributeData)?.some(list => list == 'defaultValue')} onClick={handleAdd}>
-                                <span>Add</span>
-                                <span>
-                                    {/* <s>₹5,200</s> ₹4,500 */}
-                                    <s>{getNumberWithComma(selectedProduct?.cost_price ?? 0)}</s> {getNumberWithComma(selectedProduct?.sale_price ?? 0)}
-                                </span>
-                            </button>
+                            {isCartAdd ?
+                                <button className="clear_btn add_btn">
+                                    <span>Added to Cart</span>
+                                    <span>
+                                        <svg
+                                            width="10"
+                                            height="8"
+                                            viewBox="0 0 10 8"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M9.40005 0.960205L3.92005 6.44021L1.30005 3.82021"
+                                                stroke="white"
+                                                strokeWidth="1.7"
+                                                strokeMiterlimit="10"
+                                            />
+                                        </svg>
+                                    </span>
+                                </button>
+                                :
+                                <button className={`clear_btn add_btn ${Object.values(attributeData)?.some(list => list == 'defaultValue') ? 'disabled-btn' : ''}`} disabled={Object.values(attributeData)?.some(list => list == 'defaultValue')} onClick={handleAdd}>
+                                    <span>Add</span>
+                                    <span>
+                                        {/* <s>₹5,200</s> ₹4,500 */}
+                                        <s>{getNumberWithComma(selectedProduct?.cost_price ?? 0)}</s> {getNumberWithComma(selectedProduct?.sale_price ?? 0)}
+                                    </span>
+                                </button>
+                            }
                         </div>
                     </div>
                     <div className="slaman_link display_none_in_mobile">
