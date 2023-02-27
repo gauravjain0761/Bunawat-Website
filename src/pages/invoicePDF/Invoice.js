18025a86761237400c0c726e913b29a1d8cda288
Lokeshvyascode <lokeshv.codetown@gmail.com>
2023-02-27 12:10:11 +0530

    PDF work

--- src/pages/invoicePDF/Invoice.js
+++ src/pages/invoicePDF/Invoice.js
@@ -201,30 +201,74 @@ padding: 4px;
 const html = `
     <html lang="en">
         <body>
-            <div style="border: 1px solid #000;">
-                <div style="padding: 40px 20px ;">
-                    <div style="
-                    display: flex;
-                    flex-direction: row;
-                    align-items: center;
-                    justify-content: space-between;
-                    ">
-                        <img src="https://www.doctorsplaza.in/static/media/drplazanew.4676b1446028ae3ba8d8.png" style="
-                            width: 150px;
-                            height: 70px;
-                        " />
+        <style>
+           .bunawat_doc{
+                border: 1px solid #000;
+            }
+            .bunawat_doc_top{
+                padding: 40px 20px;
+                font-size: 12px;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: space-between;
+            }
+            .bunawat_doc_top h4{
+                font-size: 14px;
+            }
+            .bunawat_doc_top p{
+                margin-top: 0px;
+                font-size: 12px;
+            }
+            .bunawat_doc_top_data{
+                display: flex; 
+                flex-direction: row;
+                justify-content: space-between;
+                padding-left: 10px;
+                padding-right: 10px;
+                align-items: flex-start;
+            }
+            .bunawat_doc_top_data p{
+                margin-top: 2px;
+                font-size: 12px;
+            }
+            table{
+                width: 100%;
+            }
+            table, th, td {
+                border: 1px solid rgb(221, 219, 219);
+                border-collapse: collapse;
+                padding: 4px;
+                font-size: 12px;
+            }
+            .bottom-data{
+                display: flex; 
+                flex-direction: row;
+                justify-content: space-between;
+                padding-left: 10px;
+                padding-right: 10px;
+                align-items: flex-start;
+                margin-bottom: 20px;
+            }
+            .bottom-data p{
+                font-size: 12px;
+                margin-bottom: 0px;
+            }
+            .bottom-data b{
+                font-weight: bold;
+            }
+        </style>
+            <div class="bunawat_doc">
+                <div class="bunawat_doc_top">
                         <div>
-                            <h4 style="
-                                margin-top: 2px;
-                                margin-bottom: 2px;
-                                font-weight: 600;
-                                font-size: 24px;
-                            ">AFPL STORES.COM PVT. LTD</h4>
-                            <p style="
-                                margin-top: 2px;
-                                font-weight: 300;
-                                font-size: 16px;
-                            ">Shop NO: 44-48, Block No: 2,<br />
+                            <img src="https://www.doctorsplaza.in/static/media/drplazanew.4676b1446028ae3ba8d8.png" style="
+                                width: 150px;
+                                height: 70px;
+                            " />
+                        </div>
+                        <div>
+                            <h4>AFPL STORES.COM PVT. LTD</h4>
+                            <p>Shop NO: 44-48, Block No: 2,<br />
                                 Busyland Complex<br />
                                 Nandgaon Peth Amravati,<br />
                                 Maharashtra, 444901 <br />
@@ -232,12 +276,10 @@ const html = `
                                 GSTIN - 27AAVCA3244L1Z6<br />
                             </p>
                         </div>
-                    </div>
                 </div>
-                <div className="row bunawat_doc_second">
-                    <div className="col-sm-6">
-                        <div className="row bunawat_doc_second_list">
-                            <div className="col-sm-6">
+                
+                <div class="bunawat_doc_top_data">
+                            <div class="">
                                 <p><span>Billing</span> <br />
                                     Avinash Thakur <br />
                                     Master infotech <br />
@@ -246,7 +288,8 @@ const html = `
                                     mohali, 160082
                                 </p>
                             </div>
-                            <div className="col-sm-6">
+                            
+                            <div className="">
                                 <p><span>Shiping</span> <br />
                                     Avinash Thakur <br />
                                     Master infotech <br />
@@ -256,26 +299,15 @@ const html = `
                                     PB 160082
                                 </p>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="row bunawat_doc_second_list">
-                            <div className="col-sm-4"><span>Invoice Number</span></div>
-                            <div className="col-sm-8"><span>: INV/2022-23/1887</span></div>
-                        </div>
-                        <div className="row bunawat_doc_second_list">
-                            <div className="col-sm-4">Order Number</div>
-                            <div className="col-sm-8">: 158673</div>
-                        </div>
-                        <div className="row bunawat_doc_second_list">
-                            <div className="col-sm-4">Order Date</div>
-                            <div className="col-sm-8">: 10 Nav, 2022</div>
-                        </div>
+                    <div class="">
+                        <div> <p>Invoice Number : INV/2022-23/1887 </p></div>
+                        <div> <p>Order Number : 158673</p></div>
+                        <div> <p>Order Date : 10 Nav, 2022</p></div>
                     </div>
                 </div>
 
-                <div className="row">
-                    <div className="col-sm-12">
+                <div class="">
+                    <div class="col-sm-12">
                         <table>
                             <tr>
                                 <th>Item</th>
@@ -294,7 +326,7 @@ const html = `
                             </tr>
                             <tr>
                                 <td>
-                                    <b>Blue skirt and top set - Blue,s</b> <br />
+                                    <b>Blue skirt and top set - Blue, s </b> <br />
                                     <span style={{ fontSize: '14px' }}>sku: LSTS3028-Ylw-BL-1</span>
                                 </td>
                                 <td>INR 3,299.11</td>
@@ -321,24 +353,179 @@ const html = `
                             </tr>
                         </table>
 
-                        <div className="row">
-                            <div className="col-sm-6"></div>
-                            <div className="col-sm-6" style={{ padding: '20px' }}>
-                                <div className="row bunawat_doc_second_list">
-                                    <div className="col-sm-4"><b>Item Subtotal:</b></div>
-                                    <div className="col-sm-8">INR 33,919.64</div>
+                        <div class="bottom-data">
+                            <div class=""></div>
+                            <div class="">
+                                <div class="">
+                                    <p><b>Item Subtotal:</b> INR 33,919.64</p>
+                                    <p><b>6% SGST:</b> 2035.18</p>
+                                    <p><b>6% CGST:</b> 2035.18</p>
+                                    <p><b>Order Total:</b> INR 37,990,00</p>
                                 </div>
-                                <div className="row bunawat_doc_second_list">
-                                    <div className="col-sm-4"><b>6% SGST:</b></div>
-                                    <div className="col-sm-8">2035.18</div>
-                                </div>
-                                <div className="row bunawat_doc_second_list">
-                                    <div className="col-sm-4"><b>6% CGST:</b></div>
-                                    <div className="col-sm-8">2035.18</div>
-                                </div>
-                                <div className="row bunawat_doc_second_list">
-                                    <div className="col-sm-4"><b>Order Total:</b></div>
-                                    <div className="col-sm-8">INR 37,990,00</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
+`;
+
+const htmlSecond = `
+    <html lang="en">
+        <body>
+        <style>
+           .bunawat_doc{
+                border: 1px solid #000;
+            }
+            .bunawat_doc_top{
+                padding: 40px 20px;
+                font-size: 12px;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: space-between;
+            }
+            .bunawat_doc_top h4{
+                font-size: 14px;
+            }
+            .bunawat_doc_top p{
+                margin-top: 0px;
+                font-size: 12px;
+            }
+            .bunawat_doc_top_data{
+                display: flex; 
+                flex-direction: row;
+                justify-content: space-between;
+                padding-left: 10px;
+                padding-right: 10px;
+                align-items: flex-start;
+            }
+            .bunawat_doc_top_data p{
+                margin-top: 2px;
+                font-size: 12px;
+            }
+            table{
+                width: 100%;
+            }
+            table, th, td {
+                border: 1px solid rgb(221, 219, 219);
+                border-collapse: collapse;
+                padding: 4px;
+                font-size: 12px;
+            }
+            .bottom-data{
+                display: flex; 
+                flex-direction: row;
+                justify-content: space-between;
+                padding-left: 10px;
+                padding-right: 10px;
+                align-items: flex-start;
+                margin-bottom: 20px;
+            }
+            .bottom-data p{
+                font-size: 12px;
+                margin-bottom: 0px;
+            }
+            .bottom-data b{
+                font-weight: bold;
+            }
+        </style>
+            <div class="bunawat_doc">
+                <div class="bunawat_doc_top">
+                        <div>
+                            <img src="https://www.doctorsplaza.in/static/media/drplazanew.4676b1446028ae3ba8d8.png" style="
+                                width: 150px;
+                                height: 70px;
+                            " />
+                        </div>
+                        <div>
+                            <h4>AFPL STORES.COM PVT. LTD</h4>
+                            <p>Shop NO: 44-48, Block No: 2,<br />
+                                Busyland Complex<br />
+                                Nandgaon Peth Amravati,<br />
+                                Maharashtra, 444901 <br />
+                                Contact - +9198608000805<br />
+                                GSTIN - 27AAVCA3244L1Z6<br />
+                            </p>
+                        </div>
+                </div>
+                
+                <div class="bunawat_doc_top_data">
+                            <div class="">
+                                <p><span>Billing</span> <br />
+                                    Avinash Thakur <br />
+                                    Master infotech <br />
+                                    12285<br />
+                                    mohali<br />
+                                    mohali, 160082
+                                </p>
+                            </div>
+                            
+                            <div className="">
+                                <p><span>Shiping</span> <br />
+                                    Avinash Thakur <br />
+                                    Master infotech <br />
+                                    12285<br />
+                                    mohali<br />
+                                    mohali, <br />
+                                    PB 160082
+                                </p>
+                            </div>
+                    <div class="">
+                        <div> <p>Invoice Number : INV/2022-23/1887 </p></div>
+                        <div> <p>Order Number : 158673</p></div>
+                        <div> <p>Order Date : 10 Nav, 2022</p></div>
+                    </div>
+                </div>
+
+                <div class="">
+                    <div class="">
+                    <table>
+                    <tr>
+                      <th>Item</th>
+                      <th>Weight</th>
+                      <th>Quantity</th>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Blue skirt and top set - Blue,s</b> <br />
+                        <span style="font-size: 14px;">sku: LSTS3028-Ylw-BL-1</span>
+                      </td>
+                      <td>N/A</td>
+                      <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>
+                          <b>Blue skirt and top set - Blue,s</b> <br />
+                          <span style="font-size: 14px;">sku: LSTS3028-Ylw-BL-1</span>
+                        </td>
+                        <td>N/A</td>
+                        <td>2</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <b>Blue skirt and top set - Blue,s</b> <br />
+                          <span style="font-size: 14px;">sku: LSTS3028-Ylw-BL-1</span>
+                        </td>
+                        <td>N/A</td>
+                        <td>3</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <b>Blue skirt and top set - Blue,s</b> <br />
+                          <span style="font-size: 14px;">sku: LSTS3028-Ylw-BL-1</span>
+                        </td>
+                        <td>N/A</td>
+                        <td>4</td>
+                      </tr>
+                  </table>
+
+                        <div class="bottom-data">
+                            <div class=""></div>
+                            <div class="">
+                                <div class="">
+                                    <p><b>Total Quantity: </b>13</p>
                                 </div>
                             </div>
                         </div>
@@ -352,7 +539,8 @@ const PdfDocument = (data) => {
     return (
         <Document>
             <Page>
-                <Html>{html}</Html>
+                {/* <Html>{html}</Html> */}
+                <Html>{htmlSecond}</Html>
             </Page>
         </Document>
     );
