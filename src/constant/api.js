05aa356dcd4a0e274a1028559ea35f6cdabbc3f4
HarikrushnR <harikrushnr.codetown@gmail.com>
2023-03-24 15:01:04 +0530

    change url

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,4 +1,4 @@
 export const API_ROUTES = {
-    BASE_URL: process.env.REACT_APP_BE_URL,
+    BASE_URL: "https://a863-2405-201-5c0a-a121-3501-4500-2510-f378.in.ngrok.io",
     API_VERSION: '/api'
 }
