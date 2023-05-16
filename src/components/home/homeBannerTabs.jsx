8ba61c2c23941465ca703fc4cdc0bff0f85c1a94
HarikrushnR <harikrushnr.codetown@gmail.com>
2023-05-16 10:54:43 +0530

    chnages

--- src/components/home/homeBannerTabs.jsx
+++ src/components/home/homeBannerTabs.jsx
@@ -34,9 +34,14 @@ const HomeBannerTabs = ({ data, singleData, setSelectedId, selectedId }) => {
                 <Tab eventKey={index} key={item?._id} title="">
                   <div className="common_home_banner">
                     {singleCollection?.mediaType == "VIDEO" ?
-                      (singleCollection?.video ? <video key={singleCollection?.video + item?._id + index} loop autoPlay muted style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'fill' }}>
-                        <source src={singleCollection?.video} type="video/mp4" />
-                      </video> : null)
+                      (singleCollection?.video ?
+                        <video key={singleCollection?.video + item?._id + index} autoPlay playsInline preload="true" src={singleCollection?.video} loop muted style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'fill' }}>
+                          <source src={singleCollection?.video} type="video/mp4"></source>
+                        </video>
+                        // <video key={singleCollection?.video + item?._id + index} loop autoPlay muted style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'fill' }}>
+                        //   <source src={singleCollection?.video} type="video/mp4" />
+                        // </video>
+                        : null)
                       :
                       (singleCollection?.image ? <img src={singleCollection?.image} alt="hero_image" style={{ position: 'absolute', width: '100%' }} /> : null)
                     }
