1f48c281ee09dc3cfd8908e2c20281e7f2d70861
HarikrushnR <harikrushnr.codetown@gmail.com>
2023-05-31 15:22:28 +0530

    changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -546,7 +546,7 @@ function Header() {
                         <Box sx={{
                             background: 'white',
                             width: { xs: '100%', sm: '400px' },
-                            height: { xs: '-webkit-calc(100vh - 60px)', sm: '80vh' },
+                            height: { xs: '-webkit-calc(100vh - 120px)', sm: '80vh' },
                             margin: { xs: '60px 0 0px 0', sm: '60px 25px 0 0' }
                         }}>
                             <NewCart data={cartData} activeHeader={getActiveHeader(scroll, hover, headerColor)} handleCartClose={handleCartClose} handleCheckout={handleCheckout} />
